e207d106ca0d515bddb5a0ca055409d124cb4a92
ChineseGame-Mu <muyihua@gmail.com>
2026-09-10 06:35:11 -0400

    fix(lobby): rebuild exact 1348x1167 data URI

--- frontend/src/cleanroom-lobby-final-image.ts
+++ frontend/src/cleanroom-lobby-final-image.ts
@@ -1,14 +1,25 @@
-import p0 from "./cleanroom-lobby-final-image-hi-part0";
-import p1 from "./cleanroom-lobby-final-image-hi-part1";
-import p2 from "./cleanroom-lobby-final-image-hi-part2";
-import p3 from "./cleanroom-lobby-final-image-hi-part3";
-import p4 from "./cleanroom-lobby-final-image-hi-part4";
-import p5 from "./cleanroom-lobby-final-image-hi-part5";
-import p6 from "./cleanroom-lobby-final-image-hi-part6";
-import p7 from "./cleanroom-lobby-final-image-hi-part7";
-import p8 from "./cleanroom-lobby-final-image-hi-part8";
-import p9 from "./cleanroom-lobby-final-image-hi-part9";
+import p00 from "./cleanroom-lobby-final-image-1348-00";
+import p01 from "./cleanroom-lobby-final-image-1348-01";
+import p02 from "./cleanroom-lobby-final-image-1348-02";
+import p03 from "./cleanroom-lobby-final-image-1348-03";
+import p04 from "./cleanroom-lobby-final-image-1348-04";
+import p05 from "./cleanroom-lobby-final-image-1348-05";
+import p06 from "./cleanroom-lobby-final-image-1348-06";
+import p07 from "./cleanroom-lobby-final-image-1348-07";
+import p08 from "./cleanroom-lobby-final-image-1348-08";
+import t00 from "./cleanroom-lobby-final-image-1348-tail00";
+import t01 from "./cleanroom-lobby-final-image-1348-tail01";
+import t02 from "./cleanroom-lobby-final-image-1348-tail02";
+import t03 from "./cleanroom-lobby-final-image-1348-tail03";
+import t04 from "./cleanroom-lobby-final-image-1348-tail04";
+import t05 from "./cleanroom-lobby-final-image-1348-tail05";
+import t06 from "./cleanroom-lobby-final-image-1348-tail06";
+import t07 from "./cleanroom-lobby-final-image-1348-tail07";
+import t08 from "./cleanroom-lobby-final-image-1348-tail08";
+import t09 from "./cleanroom-lobby-final-image-1348-tail09";
+import t10 from "./cleanroom-lobby-final-image-1348-tail10";
+import t11 from "./cleanroom-lobby-final-image-1348-tail11";
 
-const cleanroomLobbyFinalImage = `data:image/webp;base64,${p0}${p1}${p2}${p3}${p4}${p5}${p6}${p7}${p8}${p9}`;
+const cleanroomLobbyFinalImage = `data:image/webp;base64,${p00}${p01}${p02}${p03}${p04}${p05}${p06}${p07}${p08}${t00}${t01}${t02}${t03}${t04}${t05}${t06}${t07}${t08}${t09}${t10}${t11}`;
 
 export default cleanroomLobbyFinalImage;
