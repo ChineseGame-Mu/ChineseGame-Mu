2292bee5e6fd43e619decc72bbca88467ccd2891
ChineseGame-Mu <muyihua@gmail.com>
2026-09-10 06:09:27 -0400

    fix(lobby): complete 1348x1167 artwork data URI

--- frontend/src/cleanroom-lobby-final-image.ts
+++ frontend/src/cleanroom-lobby-final-image.ts
@@ -7,7 +7,8 @@ import p3c from "./cleanroom-lobby-final-image-part3c";
 import p3d from "./cleanroom-lobby-final-image-part3d";
 import p4a from "./cleanroom-lobby-final-image-part4a";
 import p4b from "./cleanroom-lobby-final-image-part4b";
+import suffix from "./cleanroom-lobby-final-image-suffix";
 
-const cleanroomLobbyFinalImage = `data:image/webp;base64,${p0}${p1}${p2}${p3a}${p3b}${p3c}${p3d}${p4a}${p4b}`;
+const cleanroomLobbyFinalImage = `data:image/webp;base64,${p0}${p1}${p2}${p3a}${p3b}${p3c}${p3d}${p4a}${p4b}${suffix}`;
 
 export default cleanroomLobbyFinalImage;
