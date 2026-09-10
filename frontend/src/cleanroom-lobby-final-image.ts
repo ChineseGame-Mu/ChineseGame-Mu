5b21af66538a239763de2ea7c939abcdbe88763c
ChineseGame-Mu <muyihua@gmail.com>
2026-09-10 06:24:50 -0400

    fix(lobby): restore verified high-res image chunks

--- frontend/src/cleanroom-lobby-final-image.ts
+++ frontend/src/cleanroom-lobby-final-image.ts
@@ -1,14 +1,14 @@
-import p0 from "./cleanroom-lobby-final-image-part0";
-import p1 from "./cleanroom-lobby-final-image-part1";
-import p2 from "./cleanroom-lobby-final-image-part2";
-import p3a from "./cleanroom-lobby-final-image-part3a";
-import p3b from "./cleanroom-lobby-final-image-part3b";
-import p3c from "./cleanroom-lobby-final-image-part3c";
-import p3d from "./cleanroom-lobby-final-image-part3d";
-import p4a from "./cleanroom-lobby-final-image-part4a";
-import p4b from "./cleanroom-lobby-final-image-part4b";
-import suffix from "./cleanroom-lobby-final-image-suffix";
+import p0 from "./cleanroom-lobby-final-image-hi-part0";
+import p1 from "./cleanroom-lobby-final-image-hi-part1";
+import p2 from "./cleanroom-lobby-final-image-hi-part2";
+import p3 from "./cleanroom-lobby-final-image-hi-part3";
+import p4 from "./cleanroom-lobby-final-image-hi-part4";
+import p5 from "./cleanroom-lobby-final-image-hi-part5";
+import p6 from "./cleanroom-lobby-final-image-hi-part6";
+import p7 from "./cleanroom-lobby-final-image-hi-part7";
+import p8 from "./cleanroom-lobby-final-image-hi-part8";
+import p9 from "./cleanroom-lobby-final-image-hi-part9";
 
-const cleanroomLobbyFinalImage = `data:image/webp;base64,${p0}${p1}${p2}${p3a}${p3b}${p3c}${p3d}${p4a}${p4b}${suffix}`;
+const cleanroomLobbyFinalImage = `data:image/webp;base64,${p0}${p1}${p2}${p3}${p4}${p5}${p6}${p7}${p8}${p9}`;
 
 export default cleanroomLobbyFinalImage;
